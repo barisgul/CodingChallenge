daa8be446f3c1b3e25868864bbd46aaee156181b
barisgul <bariss.gull@gmail.com>
2021-01-03 20:54:42 +0300

    Time Conversion

---
+++ Hackerrank/Algorithms/A08_TimeConversion/TimeConversion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Hackerrank.Algorithms.A08_TimeConversion
+{
+    //https://www.hackerrank.com/challenges/time-conversion/problem
+    public static class TimeConversion
+    {
+        #region Example Caller From Main 
+        //string s = "07:05:45PM";
+        //TimeConversion.timeConversion(s);
+        #endregion
+
+        public static string timeConversion(string time)
+        {
+            FormatType formatType;
+            string response = "";
+            
+            if (time.Contains("AM"))
+                formatType = FormatType.AM;
+            else
+                formatType = FormatType.PM;
+
+            char splitChar = formatType.ToString()[0];
+            string[] timeWithoutType = time.Split(splitChar);
+            string[] timeValues = timeWithoutType[0].Split(':');
+
+            if (formatType.Equals(FormatType.PM))
+            {
+                string newHour = "";
+                if (timeValues[0].Equals("12"))
+                    newHour = "12";
+                else
+                {
+                    newHour = (int.Parse(timeValues[0]) + 12).ToString();
+                }
+                response = newHour + ":" + timeValues[1] + ":" + timeValues[2];
+            }
+            if (formatType.Equals(FormatType.AM))
+            {
+                string newHour = "";
+                if (timeValues[0].Equals("12"))
+                {
+                    newHour = "00";
+                    response = newHour + ":" + timeValues[1] + ":" + timeValues[2];
+                    return response;
+                }
+
+                response = timeWithoutType[0];
+            }
+                
+
+            return response;
+        }
+
+        private enum FormatType
+        {
+            AM,
+            PM
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using CodingChallenge.Hackerrank.Algorithms.A05_Staircase;
 using CodingChallenge.Hackerrank.Algorithms.A06_MiniMaxSum;
 using CodingChallenge.Hackerrank.Algorithms.A07_AVeryBigSum;
+using CodingChallenge.Hackerrank.Algorithms.A08_TimeConversion;
 
 namespace CodingChallenge
 {
@@ -8,8 +9,8 @@ namespace CodingChallenge
     { 
         static void Main(string[] args)
         {
-            long[] array = new long[] { 1001458909, 1004570889, 1007019111 ,1003302837, 1002514638, 1006431461 ,1002575010 ,1007514041 ,1007548981, 1004402249 };
-            AVeryBigSum.aVeryBigSum(array);
+            string s = "07:05:45PM";
+            TimeConversion.timeConversion(s);
         }
 
     }
