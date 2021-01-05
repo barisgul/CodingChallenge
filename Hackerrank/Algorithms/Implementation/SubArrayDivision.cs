2e2861bd23eb015d4d943f44b0e64348635a6977
barisgul <bariss.gull@gmail.com>
2021-01-05 18:30:30 +0300

    subarray division cloned from https://shareablecode.com/snippets/java-solution-for-hackerrank-problem-sub-array-division-birthday-chocolate-bar-4ZtA-tbu7

---
+++ Hackerrank/Algorithms/Implementation/SubArrayDivision.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Hackerrank.Algorithms.Implementation
+{
+    //https://www.hackerrank.com/challenges/the-birthday-bar/problem
+    public class SubArrayDivision
+    {
+        public static int birthday(List<int> s, int d, int m)
+        {
+
+            int ways = 0;
+            int sum = 0;
+            //Find if there is a way to break the chocolate at all
+            if (m <= s.Count)
+                for (int i = 0; i < m; i++)
+                    sum += s[i];
+            if (sum == d) ways++;
+            ///////////////////////////////////////////////////////
+
+            //Check other possible ways to break it by using a sliding window
+            for (int i = 0; i < s.Count - m; i++)
+            {
+                sum = sum - s[i] + s[i + m];
+                if (sum == d) ways++;
+            }
+            return ways;
+        }
+    }
+}
